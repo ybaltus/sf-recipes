1bc2a8a142b9d1a61f0e80e32ca8936ff2b520e1
Yannis Baltus <yannis.baltus@outlook.com>
2023-09-16 20:07:01 +0200

    ✨ [Contact] Add a recaptcha v3

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -76,6 +78,11 @@ class ContactType extends AbstractType
                 'constraints' => [
                     new Assert\NotBlank
                 ]
+            ])
+            ->add('captcha',  Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+                'locale' => 'fr',
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
