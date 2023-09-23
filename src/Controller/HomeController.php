cb07cb8d0eacf4b9f685db7265d3aa9fd577fdc5
Yannis Baltus <yannis.baltus@outlook.com>
2023-09-23 17:32:25 +0200

    ✨ Use Cache Contracts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Repository\RecipeRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeController extends AbstractController
 {
@@ -24,8 +26,14 @@ class HomeController extends AbstractController
     #[Route('/recipe/public', name: 'recipe_index_public', methods: ['GET'])]
     public function indexRecipePublic(RecipeRepository $recipeRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $cache = new FilesystemAdapter();
+        $dataCache = $cache->get('recipes_public', function (ItemInterface $item) use ($recipeRepository): array {
+            $item->expiresAfter(10);
+            return $recipeRepository->findPublicRecipe(null);
+        });
+
         $recipes = $paginator->paginate(
-            $recipeRepository->findPublicRecipe(null),
+            $dataCache,
             $request->query->getInt('page', 1), /*page number*/
             10 /*limit per page*/
         );
